e60d4deb872b51d09ceea293260006726ea2baa8
reed <53reed53@gmail.com>
2025-10-01 22:51:27 -0400

    Backend init and schema

---
+++ backendserver/src/main/mariomonday/backend/schema/Bracket.java
@@ -0,0 +1,44 @@
+package mariomonday.backend.schema;
+
+import java.time.Instant;
+import java.util.Set;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+/**
+ * A tournament, containing multiple games
+ */
+@Data
+public class Bracket {
+
+    /**
+     * ID for the bracket
+     */
+    @Id
+    private String id;
+
+    /**
+     * The moment the bracket started
+     */
+    private Instant date;
+
+    /**
+     * The number of rounds in the bracket
+     */
+    private int rounds;
+
+    /**
+     * The winners
+     */
+    private Set<String> winners;
+
+    /**
+     * The type of game this bracket was for
+     */
+    private GameType gameType;
+
+    /**
+     * The players who participated in this bracket
+     */
+    private Set<String> players;
+}
---
+++ backendserver/src/main/mariomonday/backend/schema/GameType.java
@@ -0,0 +1,6 @@
+package mariomonday.backend.schema;
+
+public enum GameType {
+  MARIO_KART,
+  SMASH_BROS
+}
