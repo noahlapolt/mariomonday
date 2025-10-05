b01003c6578e7d3690af03d6f1b3351b73d66479
Reed Passaretti <45052736+reed53@users.noreply.github.com>
2025-10-05 14:32:43 -0400

    Added authentication, storage of Admin credentials, and complete Player API (#16)
    Signed-off-by: noahlapolt <noahlapolt@pm.me>

--- backendserver/src/main/java/mariomonday/backend/database/schema/Player.java
+++ backendserver/src/main/java/mariomonday/backend/database/schema/Player.java
@@ -2,16 +2,23 @@ package mariomonday.backend.database.schema;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * A single player, consistent across games
  */
 @Data
+@Document
 public class Player {
 
     /** Player id */
     @Id
     private String id;
 
+    /**
+     * Player name, must be unique
+     */
+    @Indexed(unique = true)
     private String name;
 }
