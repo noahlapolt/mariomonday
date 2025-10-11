119560c0901aa0caaf9c68a7815fdaf323154a24
Zachary DiMarco <zadimarco@gmail.com>
2025-10-11 02:06:43 -0400

    Adding prettier-java

--- backendserver/src/test/java/mariomonday/backend/database/schemas/GameSetTests.java
+++ backendserver/src/test/java/mariomonday/backend/database/schemas/GameSetTests.java
@@ -1,6 +1,10 @@
 package mariomonday.backend.database.schemas;
 
+import java.util.Set;
 import java.util.stream.Stream;
+import mariomonday.backend.database.schema.GameSet;
+import mariomonday.backend.database.schema.GameType;
+import mariomonday.backend.utils.TestDataUtil;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,15 +12,11 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import mariomonday.backend.database.schema.GameSet;
-import mariomonday.backend.database.schema.GameType;
-
 @SpringBootTest
 class GameSetTests {
 
   @Test
-  void contextLoads() {
-  }
+  void contextLoads() {}
 
   private static Stream<Arguments> isByeRound() {
     return Stream.of();
@@ -25,14 +25,8 @@ class GameSetTests {
   @ParameterizedTest
   @MethodSource
   void isByeRound(GameType gameType, int playersInGame1, int playersInGame2, int playersDirectlyAdded) {
-    GameSet testGame = GameSet.builder().previousGameSets().build();
-
+    GameSet testGame = GameSet.builder().previousGameSets(
+      Set.of(TestDataUtil.createFakeGameSet().playerSets(TestDataUtil.createNFakePlayers(playersInGame1))).build()
+    );
   }
-
-
-  private GameSet createTestGameSet(int numberOfPlayers) {
-    return GameSet.builder().build();
-  }
-
-
 }
