cd1dc71002e25f7376908b913328b4005a4f1548
reed <53reed53@gmail.com>
2025-10-16 19:49:49 -0400

    Backend builds again

--- backendserver/src/main/java/mariomonday/backend/database/tables/GameRepository.java
+++ backendserver/src/main/java/mariomonday/backend/database/tables/GameRepository.java
@@ -8,27 +8,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * Table for games
  */
 public interface GameRepository extends MongoRepository<Game, String> {
-  /**
-   * Query games by set ID
-   *
-   * @param setId The ID of the set
-   * @return The games associated with the given set
-   */
-  List<Game> findBySetId(String setId);
-
-  /**
-   * Query games by participants
-   *
-   * @param playerId The player
-   * @return The games that player has played
-   */
-  List<Game> findByPlayersContains(String playerId);
-
-  /**
-   * Query games by winners
-   *
-   * @param playerId The player
-   * @return The games that player has won
-   */
-  List<Game> findByWinnersContains(String playerId);
 }
